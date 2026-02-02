c0914dfa0f4165d870ccbed505b97e6e40803132
ianflexa <ianflexa@gmail.com>
2026-02-02 16:17:08 +0100

    test: fix zksync tests

--- tests/zksync/rsETHPriceCapAdapterZkSyncTest.t.sol
+++ tests/zksync/rsETHPriceCapAdapterZkSyncTest.t.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.8.0;
 import '../BaseTest.sol';
 
 import {CLRatePriceCapAdapter} from '../../src/contracts/CLRatePriceCapAdapter.sol';
-import {CapAdaptersCodeZkSync} from '../../scripts/DeployZkSync.s.sol';
+import {CapAdaptersCodeZkSync, AaveV3ZkSyncAssets, AaveV3ZkSync} from '../../scripts/DeployZkSync.s.sol';
 
 contract rsETHPriceCapAdapterZkSyncTest is BaseTest {
   constructor()
     BaseTest(
       CapAdaptersCodeZkSync.rsETHAdapterParams(),
-      5,
-      ForkParams({network: 'zksync', blockNumber: 58320360}),
+      14,
+      ForkParams({network: 'zksync', blockNumber: 60120360}),
       'rsETH_ZkSync'
     )
   {}
