fc001292c3fc14ba2c454c75f78022186a53a416
ianflexa <ianflexa@gmail.com>
2026-02-05 12:49:03 +0100

    test: refactor to consolidate multi-network asset per asset-file

---
+++ tests/adapters/LBTCPriceCapAdapterTest.t.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import '../BaseTest.sol';
+
+import {LBTCPriceCapAdapter, IPriceCapAdapter} from '../../src/contracts/lst-adapters/LBTCPriceCapAdapter.sol';
+import {CapAdaptersCodeBase} from '../../scripts/DeployBase.s.sol';
+import {CapAdaptersCodeEthereum} from '../../scripts/DeployEthereum.s.sol';
+import {ILBTC} from '../../src/interfaces/ILBTC.sol';
+
+contract LBTCEthereumTest is BaseTest {
+  constructor()
+    BaseTest(
+      CapAdaptersCodeEthereum.lBTCAdapterCode(),
+      7,
+      ForkParams({network: 'mainnet', blockNumber: 23033850}),
+      'LBTC_Ethereum'
+    )
+  {}
+
+  function _mockRatioProviderRate(uint256 amount) internal override {
+    vm.mockCall(
+      CapAdaptersCodeEthereum.LBTC_STAKE_ORACLE,
+      abi.encodeWithSelector(ILBTC.getRate.selector),
+      abi.encode(amount)
+    );
+  }
+
+  function _createAdapter(
+    IPriceCapAdapter.CapAdapterParams memory capAdapterParams
+  ) internal override returns (IPriceCapAdapter) {
+    return new LBTCPriceCapAdapter(capAdapterParams);
+  }
+}
+
+contract LBTCBaseTest is BaseTest {
+  constructor()
+    BaseTest(
+      CapAdaptersCodeBase.lBTCAdapterCode(),
+      7,
+      ForkParams({network: 'base', blockNumber: 33556903}),
+      'LBTC_Base'
+    )
+  {}
+
+  function _mockRatioProviderRate(uint256 amount) internal override {
+    vm.mockCall(
+      CapAdaptersCodeBase.LBTC_STAKE_ORACLE,
+      abi.encodeWithSelector(ILBTC.getRate.selector),
+      abi.encode(amount)
+    );
+  }
+
+  function _createAdapter(
+    IPriceCapAdapter.CapAdapterParams memory capAdapterParams
+  ) internal override returns (IPriceCapAdapter) {
+    return new LBTCPriceCapAdapter(capAdapterParams);
+  }
+}
---
+++ tests/adapters/sUSDePriceCapAdapterTest.t.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import '../BaseTest.sol';
+import {CLAdapterBaseTest} from '../CLAdapterBaseTest.sol';
+
+import {SUSDePriceCapAdapter} from '../../src/contracts/lst-adapters/SUSDePriceCapAdapter.sol';
+import {CapAdaptersCodeAvalanche} from '../../scripts/DeployAvalanche.s.sol';
+import {CapAdaptersCodeEthereum} from '../../scripts/DeployEthereum.s.sol';
+import {CapAdaptersCodeMantle} from '../../scripts/DeployMantle.s.sol';
+import {CapAdaptersCodePlasma} from '../../scripts/DeployPlasma.s.sol';
+
+// was tested with USDe / USD feed for a longer period
+contract sUSDeEthereumTest is BaseTest {
+  constructor()
+    BaseTest(
+      CapAdaptersCodeEthereum.sUSDeAdapterCode(),
+      8,
+      ForkParams({network: 'mainnet', blockNumber: 20131922}),
+      'sUSDe_Ethereum'
+    )
+  {}
+
+  function _createAdapter(
+    IPriceCapAdapter.CapAdapterParams memory capAdapterParams
+  ) internal override returns (IPriceCapAdapter) {
+    return new SUSDePriceCapAdapter(capAdapterParams);
+  }
+}
+
+contract sUSDeMantleTest is CLAdapterBaseTest {
+  constructor()
+    CLAdapterBaseTest(
+      CapAdaptersCodeMantle.sUSDeAdapterCode(),
+      3,
+      ForkParams({network: 'mantle', blockNumber: 90952191}),
+      'sUSDe_Mantle'
+    )
+  {}
+}
+
+contract sUSDeAvalancheTest is CLAdapterBaseTest {
+  constructor()
+    CLAdapterBaseTest(
+      CapAdaptersCodeAvalanche.sUSDeAdapterCode(),
+      30,
+      ForkParams({network: 'avalanche', blockNumber: 77096662}),
+      'sUSDe_Avalanche'
+    )
+  {}
+}
+
+contract sUSDePlasmaTest is CLAdapterBaseTest {
+  constructor()
+    CLAdapterBaseTest(
+      CapAdaptersCodePlasma.sUSDeAdapterParams(),
+      14,
+      ForkParams({network: 'plasma', blockNumber: 3764593}),
+      'sUSDe_Plasma'
+    )
+  {}
+}
