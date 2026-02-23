ce4e287560ec22a7fe64d809407d93305b9e8e67
Ian Flexa <85500650+ianflexa@users.noreply.github.com>
2026-02-23 17:21:52 +0100

    refactor: reorg tests per asset with multiple networks (#133)
    * test: fix sUSDe avalanche
    
    * test: fix lbtc tests
    
    * test: fix Ink tests
    
    * test: fix wstETH linea
    
    * test: fix mantle tests
    
    * test: fix plasma tests
    
    * test: fix zksync tests
    
    * test: fix mainnet
    
    * test: more mainnet fixes
    
    * fix zksync path
    
    * fix: remove deprecated tests
    
    * fix: remove global exclude lint
    
    * chore: bump libs
    
    * using svr from address-book again
    
    * reorg tests per asset with multiple networks
    
    * test: refactor to consolidate multi-network asset per asset-file
    
    * chore: makefile generic deployment commands
    
    * Apply suggestions from code review
    
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>
    
    ---------
    
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>

---
+++ tests/adapters/wstETHPriceCapAdapterTest.t.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import '../BaseTest.sol';
+import {CLAdapterBaseTest} from '../CLAdapterBaseTest.sol';
+
+import {CLRatePriceCapAdapter} from '../../src/contracts/CLRatePriceCapAdapter.sol';
+import {WstETHPriceCapAdapter} from '../../src/contracts/lst-adapters/WstETHPriceCapAdapter.sol';
+import {CapAdaptersCodeBNB} from '../../scripts/DeployBnb.s.sol';
+import {CapAdaptersCodeEthereum} from '../../scripts/DeployEthereum.s.sol';
+import {CapAdaptersCodeInk} from '../../scripts/DeployInk.s.sol';
+import {CapAdaptersCodeLinea} from '../../scripts/DeployLinea.s.sol';
+import {CapAdaptersCodePlasma} from '../../scripts/DeployPlasma.s.sol';
+import {CapAdaptersCodeMegaEth} from '../../scripts/DeployMegaEth.s.sol';
+
+contract wstETHEthereumTest is BaseTest {
+  constructor()
+    BaseTest(
+      CapAdaptersCodeEthereum.wstETHAdapterCode(),
+      30,
+      ForkParams({network: 'mainnet', blockNumber: 24231100}),
+      'wstETH_Ethereum'
+    )
+  {}
+
+  function _createAdapter(
+    IPriceCapAdapter.CapAdapterParams memory capAdapterParams
+  ) internal override returns (IPriceCapAdapter) {
+    return new WstETHPriceCapAdapter(capAdapterParams);
+  }
+}
+
+contract wstETHBNBTest is BaseTest {
+  constructor()
+    BaseTest(
+      CapAdaptersCodeBNB.wstETHAdapterCode(),
+      9,
+      ForkParams({network: 'bnb', blockNumber: 42738754}),
+      'wstETH_BNB'
+    )
+  {}
+
+  function _createAdapter(
+    IPriceCapAdapter.CapAdapterParams memory capAdapterParams
+  ) internal override returns (IPriceCapAdapter) {
+    return new CLRatePriceCapAdapter(capAdapterParams);
+  }
+}
+
+contract wstETHLineaTest is BaseTest {
+  constructor()
+    BaseTest(
+      CapAdaptersCodeLinea.wstETHAdapterCode(),
+      14,
+      ForkParams({network: 'linea', blockNumber: 15415372}),
+      'wstETH_Linea'
+    )
+  {}
+
+  function _createAdapter(
+    IPriceCapAdapter.CapAdapterParams memory capAdapterParams
+  ) internal override returns (IPriceCapAdapter) {
+    return new CLRatePriceCapAdapter(capAdapterParams);
+  }
+}
+
+contract wstETHInkTest is CLAdapterBaseTest {
+  constructor()
+    CLAdapterBaseTest(
+      CapAdaptersCodeInk.wstETHAdapterCode(),
+      14,
+      ForkParams({network: 'ink', blockNumber: 32384403}),
+      'wstETH_Ink'
+    )
+  {}
+}
+
+contract wstETHPlasmaTest is CLAdapterBaseTest {
+  constructor()
+    CLAdapterBaseTest(
+      CapAdaptersCodePlasma.wstETHAdapterCode(),
+      14,
+      ForkParams({network: 'plasma', blockNumber: 6119800}),
+      'wstETH_plasma'
+    )
+  {}
+}
+
+contract wstETHMegaEthTest is CLAdapterBaseTest {
+  constructor()
+    CLAdapterBaseTest(
+      CapAdaptersCodeMegaEth.wstEthAdapterCode(),
+      0,
+      ForkParams({network: 'megaeth', blockNumber: 7304587}),
+      'wstETH_megaEth'
+    )
+  {}
+
+  function test_latestAnswerRetrospective() public pure override {
+    // cannot test due to newly base feed deployed
+    assertTrue(true);
+  }
+}
